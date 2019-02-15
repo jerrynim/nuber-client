27f01396c76cd179c33ed16c9ce0a0e38ba8fada
jerrynim <tjerry3@naver.com>
2019-02-15 14:58:39 +0900

    git

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { graphql } from "react-apollo";
 import { IS_LOGGED_IN } from "./AppQueries";
 
-/* data 는 쿼리로 부터의 데이터 */
+/* data 는 쿼리로 부터의 데이터  */
 const AppContainer = ({ data }) => <div>{JSON.stringify(data)}</div>;
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
