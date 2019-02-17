03e7830ad67cd07354f2f494de03e2a0093f96f3
jerrynim <tjerry3@naver.com>
2019-02-17 18:54:03 +0900

    react-toastify install

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { graphql } from "react-apollo";
+import { ToastContainer } from "react-toastify";
 import theme from "../../theme";
 import { ThemeProvider } from "../../typed-components";
 import AppPresenter from "./AppPresenter";
@@ -9,8 +10,11 @@ import { IS_LOGGED_IN } from "./AppQueries";
 /*data = {"variables":{},"loading":false,"networkStatus":7,"auth":{"isLoggedIn":false,"__typename":"Auth"}} */
 // tslint:disable-next-line
 const AppContainer = ({ data }) => (
-  <ThemeProvider theme={theme}>
-    <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
-  </ThemeProvider>
+  <React.Fragment>
+    <ThemeProvider theme={theme}>
+      <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+    </ThemeProvider>
+    <ToastContainer draggable={true} position={"bottom-center"} />
+  </React.Fragment>
 );
 export default graphql(IS_LOGGED_IN)(AppContainer);
