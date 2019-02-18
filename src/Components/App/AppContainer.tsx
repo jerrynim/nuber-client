0640991292916641045a87f8a99b0e64da259975
jerrynim <tjerry3@naver.com>
2019-02-18 19:21:12 +0900

    facebook login + sidebar

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -5,7 +5,7 @@ import "react-toastify/dist/ReactToastify.min.css";
 import theme from "../../theme";
 import { ThemeProvider } from "../../typed-components";
 import AppPresenter from "./AppPresenter";
-import { IS_LOGGED_IN } from "./AppQueries";
+import { IS_LOGGED_IN } from "./AppQueries.local";
 
 /* data 는 쿼리로 부터의 데이터  */
 /*data = {"variables":{},"loading":false,"networkStatus":7,"auth":{"isLoggedIn":false,"__typename":"Auth"}} */
